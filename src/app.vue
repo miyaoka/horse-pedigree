c30763ddfb833422e53571af1b331ab76f49a9ed
Masaya Kazama <miyaokamiyao@gmail.com>
2023-05-03 13:50:52 +0900

    WIP

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: "Horse pedigree",
+  meta: [
+    {
+      name: "description",
+      content: "スプレッドシートから系統図を表示するツール",
+    },
+  ],
+});
+</script>
+
 <template>
   <HorseEditor />
 </template>
